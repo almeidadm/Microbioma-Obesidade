daa5f0e4eb49dfe4786e8ed16f609ea2d8e1a6b9
diego <almeida.dm@unifesp.br>
2020-08-28 17:26:07 -0300

    Utilização da biblioteca Pandas para processar o formato adequado dos arquivos

--- SVM.py
+++ SVM.py
@@ -1,4 +1,4 @@
-from csv import reader
+import pandas as pd
 import timeit
 from sklearn.model_selection import train_test_split
 from sklearn import svm
@@ -6,14 +6,14 @@ from sklearn import metrics
 from sklearn.metrics import confusion_matrix
 
 
+
 # importar dados formato csv/data
-def load_csv(filename):
-    file = open(filename, "r")
-    lines = reader(file)
-    dataset = list(lines)
-    for i in range(len(dataset)):
-        dataset[i] = dataset[i][:-1]
-    file.close()
+def format_file(filename):
+    data = pd.read_csv(filename, sep=",", low_memory=False)
+    clm = list(data.columns[1:-1])
+    dataset = list()
+    for sample in clm:
+        dataset.append(list(data[sample]))
     return dataset
 
 
@@ -28,7 +28,7 @@ def split_label(dataset, c):
 # transformar colunas de string para Float
 def str_column_to_float(dataset, column):
     for row in dataset:
-        row[column] = float(row[column].strip())
+        row[column] = float(row[column])
 
 
 # transformar colunas de string para Int
@@ -43,40 +43,17 @@ def strColumn_toInt(dataset, column):
         row[column] = lookup[row[column]]
     return lookup
 
-
-def reformat_dataset(dataset, row):
-    label = dict()
-    # get index of each labeled sample
-    for i in range(1, len(dataset[row])):
-        if dataset[row][i] not in label:
-            label[dataset[row][i]] = [i]
-        else:
-            label[dataset[row][i]].append(i)
-    new_dataset = list()
-    # append each sample in your respective label into the dict
-    for key in label.keys():
-        for i in label[key]:
-            sample = []
-            for j in range(1, len(dataset)):
-                sample.append(dataset[j][i])
-            sample.append(key)
-            new_dataset.append(sample)
-    return new_dataset
-
-
 if __name__ == '__main__':
     start = timeit.default_timer()
 
-    dataset = load_csv("./data/obesity_abundance.txt")
-    dataset = reformat_dataset(dataset[2:], 0)
+    dataset = format_file("./data/obesity_abundance.txt")
 
-    for i in range(len(dataset[0]) - 1):
+    for i in range(1, len(dataset[0])):
         str_column_to_float(dataset, i)
 
-    strColumn_toInt(dataset, -1)
+    strColumn_toInt(dataset, 0)
 
-    dataset, label = split_label(dataset, -1)
-
+    dataset, label = split_label(dataset, 0)
     # dividindo dataset em treino e teste
     data_train, data_test, label_train, label_test = train_test_split(dataset, label, test_size=0.3)
 
@@ -89,6 +66,8 @@ if __name__ == '__main__':
     # predizendo para o dataset
     y_pred = clf.predict(data_test)
 
+    print("Number of samples: ", len(dataset))
+    print("Number of features: ", len(dataset[0]))
     print("Accuracy: ", metrics.accuracy_score(label_test, y_pred))
     print("Precision (average=macro): ", metrics.precision_score(label_test, y_pred, average='macro'))
     print("Recall (average=macro): ", metrics.recall_score(label_test, y_pred, average='macro'))
